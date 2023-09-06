740f751ec610d7e2588d3786f65a1477b9626fb0
atifrit <adamtifrit@gmail.com>
2023-09-06 13:48:09 -0700

    added get all spots router

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,14 +9,51 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+// Get all spots
+
   router.get('/', async (req, res) => {
     let spots = await Spot.findAll({include: [
-      { model:Review,
-        attributes:['stars']
-      }
+      {model:Review},
+      {model:SpotImage}
     ]});
 
-    res.send(spots);
-  })
+    let spotsPOJOs = spots.map(spot => spot.toJSON());
+
+    console.log(spotsPOJOs);
+    spotsPOJOs.map(spot => {
+      let reviews = spot.Reviews;
+      let count = reviews.length;
+      let starCount = 0;
+      for(let el of reviews) {
+        starCount += el.stars;
+      }
+
+      spot.avgRating = starCount/count;
+      delete spot.Reviews;
+    })
+
+
+    spotsPOJOs.map(spot => {
+      let spotImages = spot.SpotImages;
+      let previewImage = null;
+      for(let el of spotImages) {
+        if(el.preview == true) {
+          previewImage = el.url;
+        }
+      }
+
+      spot.preview = previewImage;
+      delete spot.SpotImages;
+    })
+
+    res.send(spotsPOJOs);
+  });
+
+// End of Get all spots
+
+
+
+
+
 
 module.exports = router;
