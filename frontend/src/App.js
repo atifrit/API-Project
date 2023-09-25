4d1235e8edc3d20de4fcdd8f7b8d25ab75d26a74
atifrit <adamtifrit@gmail.com>
2023-09-25 08:38:39 -0700

    added comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
+
+// checking
+
+
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
